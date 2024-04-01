82c1674c14098256a21b02460669a092ab3f3155
ramabhadrarao <ramabhadrarao.maddu@yahoo.com>
2024-04-01 11:04:07 +0530

    new file

---
+++ DisplayTeluguChars.java
@@ -0,0 +1,9 @@
+public class DisplayTeluguChars {
+    public static void main(String[] args) {
+        // Telugu text to display
+        String teluguText = "స్వర్ణాంధ్ర ఇంజనీరింగ్ మరియు సాంకేతిక పాఠశాల, సీతారాంపురం, నర్సాపూర్, ఆంధ్రప్రదేశ్ 534280, భారతదేశం";
+
+        // Print Telugu text to the console
+        System.out.println(teluguText);
+    }
+}
