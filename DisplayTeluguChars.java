d034511a10febb844289405ebed167f4c2c331b3
ramabhadrarao <ramabhadrarao.maddu@yahoo.com>
2024-04-01 11:19:48 +0530

    update code in DisplayTeluguChars

--- DisplayTeluguChars.java
+++ DisplayTeluguChars.java
@@ -1,7 +1,11 @@
 public class DisplayTeluguChars {
     public static void main(String[] args) {
-        // Telugu text to display
-        String teluguText = "స్వర్ణాంధ్ర ఇంజనీరింగ్ మరియు సాంకేతిక పాఠశాల, సీతారాంపురం, నర్సాపూర్, ఆంధ్రప్రదేశ్ 534280, భారతదేశం";
+        // Telugu text in Unicode escape sequences
+        String teluguText = "\u0C38\u0C4D\u0C35\u0C3E\u0C30\u0C02\u0C21\u0C4D\u0C30 "
+                + "\u0C05\u0C02\u0C1C\u0C02\u0C30\u0C40\u0C2A\u0C4D\u0C30\u0C38\u0C4D\u0C2E\u0C30\u0C3E\u0C02\u0C2E\u0C4D\u0C2F\u0C41"
+                + "\u0C15\u0C4D\u0C15\u0C3E\u0C24\u0C3E\u0C02, \u0C38\u0C40\u0C2E\u0C4D\u0C1F\u0C3E\u0C30\u0C3E\u0C02\u0C2A\u0C41\u0C30\u0C3E"
+                + "\u0C2E\u0C3F\u0C30\u0C3E\u0C02, \u0C28\u0C30\u0C4D\u0C38\u0C3E\u0C2A\u0C38\u0C4D\u0C30\u0C4D 534280, \u0C2D\u0C3E\u0C30\u0C24"
+                + "\u0C26\u0C47\u0C36\u0C4D";
 
         // Print Telugu text to the console
         System.out.println(teluguText);
